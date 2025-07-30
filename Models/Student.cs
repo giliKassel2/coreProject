e169f93a212177898e39c3455f11cde2a93e54d3
giliKassel2 <gilli055672@gmail.com>
2025-07-30 22:17:29 +0300

    smal changes

--- Models/Student.cs
+++ Models/Student.cs
@@ -4,7 +4,7 @@ using myProject.Models;
 public class Student
 {
   // internal int id;
-
+ public string HashPassword {get; set;}
   public int Id { get; set; }
   public string Name { get; set; }
   public List<Presence> presence { get; set; }
